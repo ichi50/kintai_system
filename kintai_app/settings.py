9c91f8bb63e213882a5e558d77a36c8e269e7207
koki <kouki.kouki0519@gmail.com>
2025-10-30 09:51:57 +0900

    feat: configure production db settings

--- kintai_app/settings.py
+++ kintai_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e6h1k$0^*e-9uxf7iakn%1lr9f#$0s9&2_w(x1hp+e$svgzrwb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.239.16.133']
 
 
 # Application definition
